4a06de1b6b1fa84ddd2d41cdef4eb362f7ffddb2
rmilenko <45405257+rmilenko@users.noreply.github.com>
2019-06-18 10:40:21 +0200

    Updated response message

--- main.go
+++ main.go
@@ -14,7 +14,7 @@ func main() {
 
 		f := fib()
 
-		res := &response{Message: "Hello World"}
+		res := &response{Message: "Pera Kojot, suvi genije!!!"}
 
 		for _, e := range os.Environ() {
 			pair := strings.Split(e, "=")
